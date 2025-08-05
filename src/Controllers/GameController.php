f1871f005f838f5be0100650a2fa0aa594092105
jhleeseoul <jhlee2483@gmail.com>
2025-08-05 20:27:26 +0900

    import pawn move test in gamecontroller

--- src/Controllers/GameController.php
+++ src/Controllers/GameController.php
@@ -7,15 +7,20 @@ use App\Models\ChessLogic;
 class GameController
 {
     // 임시 테스트용 메소드
-    public function testFenParser(): void
+    public function testPieceMoves(): void
     {
-        // 체스 초기 상태 FEN
+        // 백의 폰 e2가 움직일 수 있는 위치 테스트
         $fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+        $coord = 'e2';
         
         $logic = new ChessLogic($fen);
-        $board = $logic->getBoard();
+        $validMoves = $logic->getValidMovesForPiece($coord);
 
         header('Content-Type: application/json');
-        echo json_encode($board);
+        echo json_encode([
+            'fen' => $fen,
+            'piece_at' => $coord,
+            'valid_moves' => $validMoves
+        ]);
     }
 }
